35152a9563407ad3499947da5fae287cd0d2c061
Spencer Vaughn <svaughn57@gmail.com>
2024-09-22 21:51:53 -0500

    Refactor window title and add count button functionality

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 #include "CloseButton.h"
 #include "MainWindow.h"
 
+int count = 0;
+
+void on_button_count_press(GtkWidget *widget, gpointer ptr)
+{
+    char buffer[30];
+    count++;
+    sprintf(buffer, "Button Pressed %d times", count);
+    gtk_label_set_text(GTK_LABEL(ptr), buffer);
+}
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
@@ -9,7 +19,16 @@ int main(int argc, char *argv[])
     GtkWidget *window = create_main_window();
     GtkWidget *close_button = create_close_button(G_CALLBACK(on_window_closed));
 
-    gtk_container_add(GTK_CONTAINER(window), close_button);
+    GtkWidget *label = gtk_label_new("My Label");
+    GtkWidget *count_button = gtk_button_new_with_label("Count button");
+    g_signal_connect(count_button, "clicked", G_CALLBACK(on_button_count_press), label);
+    
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(box), count_button, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), label, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(box), close_button, TRUE, TRUE, 0);
+
+    gtk_container_add(GTK_CONTAINER(window), box);
     gtk_widget_show_all(window);
 
     gtk_main();
